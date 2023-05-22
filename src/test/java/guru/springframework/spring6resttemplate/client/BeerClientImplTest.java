73742f1ddc679b1bf21de674851acde95c0bcd7c
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-22 17:49:27 -0500

    Add getBeerById

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -34,5 +38,16 @@ class BeerClientImplTest {
     @Test
     void testListBeersShowInventory() {
         beerClient.listBeers("ALE", null, true, null, null);
+    }
+
+    @Test
+    void testGetBeerById() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(byId);
     }
 }
